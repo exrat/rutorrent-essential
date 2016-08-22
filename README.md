4b1ca5127febbaf2d2ab79c2dde448352b35c92c
exrat <ex_rat@hotmail.com>
2016-08-22 09:10:32 +0200

    passage plugin irssi en "enabled=no" la conf reste en place et tourne en fond mais passage obligé en plugins.ini pour l'activer comme pour le plugin chat.

--- README.md
+++ README.md
@@ -67,9 +67,7 @@ Pour gérer vos utilisateurs ultérieurement, il vous suffit de relancer le scri
 ### Disclaimer
 Ce script est proposé à des fins d'expérimentation uniquement, le téléchargement d’oeuvre copyrightées est illégal.
 
-Merci de vous conformer à la législation en vigueur en fonction de vos pays respectifs en faisant vos tests sur des fichiers libres de droit.
-
-De plus, aucun animal n’a été maltraité durant la laborieuse phase de dévellopement !
+Merci de vous conformer à la législation en vigueur en fonction de vos pays respectifs en faisant vos tests sur des fichiers libres de droits.
 
 ### License
 This work is licensed under a [Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License](http://creativecommons.org/licenses/by-nc-sa/4.0/)
--- essential.sh
+++ essential.sh
@@ -6,17 +6,16 @@
 # Nécessite Debian 7 ou 8 (32/64 bits) & un serveur fraîchement installé
 #
 # Multi-utilisateurs
-# Inclus VsFTPd (ftp & ftps sur le port 21), Fail2ban (avec conf nginx, ftp & ssh) & Proxy php
+# Inclus VsFTPd (ftp & ftps sur le port 21), Fail2ban (avec conf nginx, ftp & ssh)
 # Seedbox-Manager, Auteurs: Magicalex, Hydrog3n et Backtoback
 #
 # Tiré du tutoriel de Magicalex pour mondedie.fr disponible ici:
 # http://mondedie.fr/viewtopic.php?id=5302
-# Aide, support & plus si affinités à la même adresse ! http://mondedie.fr/
 #
 # Merci Aliochka & Meister pour les conf de Munin et VsFTPd
 # à Albaret pour le coup de main sur la gestion d'users, LetsGo67 pour ses rectifs et
 # Jedediah pour avoir joué avec le html/css du thème.
-# Aux traducteurs: Sophie, Spectre, Hardware, Zarev.
+# Aux traducteurs: Sophie, Spectre, Hardware, Zarev, SirGato, MiguelSam.
 #
 # Installation:
 #
