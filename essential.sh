3fe0ba598e9081f9824b725be43c1b2db473e018
exrat <francois.barcelo@gmail.com>
2018-10-23 10:40:57 +0200

    fix pass log

--- essential.sh
+++ essential.sh
@@ -679,6 +679,8 @@ if [ ! -f "$NGINXENABLE"/rutorrent.conf ]; then
 		if FONCNO "$REPONSE"; then
 			# fin d'installation
 			echo ""; set "192"; FONCTXT "$1"; echo -e "${CBLUE}$TXT1${CEND}"
+			CLEANPASS="$(grep 182 "$ESSENTIAL"/lang/"$GENLANG".lang | cut -c5- | sed "s/.$//")"
+			sed -i "/$CLEANPASS/,+4d" /tmp/install.log
 			cp -f /tmp/install.log "$RUTORRENT"/install.log
 			ccze -h < "$RUTORRENT"/install.log > "$RUTORRENT"/install.html
 			true > /var/log/nginx/rutorrent-error.log
